9f65768b46ab2853d2f980c2373e618a1ef7117e
Nayara Silva <nayara.silva.f@hotmail.com>
2018-08-08 20:48:44 -0300

    encapsulamento

--- eclipse-workspace/livraria/src/livraria/CadastroDeLivros.java
+++ eclipse-workspace/livraria/src/livraria/CadastroDeLivros.java
@@ -5,32 +5,31 @@ public class CadastroDeLivros {
 	public static void main(String[] args) {
 		
 		Autor autor = new Autor();
-		autor.nome = "Rodrigo Turini";
-		autor.email = "rodrigo.turini@caelum.com.br";
-		autor.cpf = "123.456.789-10";
+		autor.setNome("Rodrigo Turini");
+		autor.setEmail("rodrigo.turini@caelum.com.br");
+		autor.setCpf("123.456.789-10");
 	
 		
-		Livro livro = new Livro();
-		livro.nome = "Java 8 Prática";
-		livro.descricao = "Novos recursos da linguagem";
-		livro.valor = 59.90;
-		livro.isbn = "978-85-66250-46-6";
-		livro.autor = autor;
+		Livro livro = new Livro(autor);
+		livro.setNome("Java 8 Prática");
+		livro.setDescricao("Novos recursos da linguagem");
+		livro.setValor(59.90);
+		livro.setIsbn("978-85-66250-46-6");
 		
 		livro.mostrarDetalhes();
 		
 		Autor outroAutor = new Autor();
-		outroAutor.nome = "Paulo Silvera";
-		outroAutor.email = "paulosilvera@caelum.com.br";
-		outroAutor.cpf = "123.456.789-10";
+		outroAutor.setNome("Paulo Silvera");
+		outroAutor.setEmail("paulosilvera@caelum.com.br");
+		outroAutor.setCpf("123.456.789-10");
 		
 		
-		Livro outroLivro = new Livro();
-		outroLivro.nome = "Lógica de Programação";
-		outroLivro.descricao = "Criar seus primeiros programas";
-		outroLivro.valor = 59.90;
-		outroLivro.isbn = "978-85-66250-22-0";
-		outroLivro.autor = outroAutor;
+		Livro outroLivro = new Livro(outroAutor);
+		outroLivro.setNome("Lógica de Programação");
+		outroLivro.setDescricao("Criar seus primeiros programas");
+		outroLivro.setValor(59.90);
+		outroLivro.setIsbn("978-85-66250-22-0");
+		outroLivro.setAutor(outroAutor);
 		
 		outroLivro.mostrarDetalhes();
 		
